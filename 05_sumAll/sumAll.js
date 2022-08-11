a945ff7e13d8122440f5c178e8500af52f1d08b6
O.Remilekun <remmylknolaotan@gmail.com>
2022-08-12 00:16:27 +0100

    Add completed 05_sumAll challenge

---
+++ 05_sumAll/sumAll.js
@@ -0,0 +1,20 @@
+const sumAll = function(n1, n2) {
+let sum = 0;
+if ((typeof n1 !== 'number' || typeof n2 !== 'number') ||
+    (n1 < 0 || n2 < 0)) {
+    return 'ERROR';
+    } else if (n1 > n2) {
+        for (let i = n2; i <= n1; i++) {
+            sum += i;
+            }
+        return sum;
+    } else {
+        for (let i = n1; i <= n2; i++) {
+            sum += i;
+            }
+        return sum;
+    }
+};
+
+// Do not edit below this line
+module.exports = sumAll;
