84ef99800a6628f2a46df6d7ed7e254b30fb6918
O.Remilekun <remmylknolaotan@gmail.com>
2022-08-12 00:13:10 +0100

    Add completed 02 repeatString challenge

---
+++ 02_repeatString/repeatString.js
@@ -0,0 +1,15 @@
+
+const repeatString = function(str, num) {
+    let result = '';
+    if (num < 0) return 'ERROR';
+    if (!str)  return '';
+
+    for (let i = 0; i < num; i++) {
+        result += str;
+    }
+    return result;
+};
+repeatString('hey', 3)
+
+// Do not edit below this line
+module.exports = repeatString;
