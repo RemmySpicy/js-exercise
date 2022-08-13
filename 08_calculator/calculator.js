f5d5be2ece014559b64a3436340948860c183209
O.Remilekun <remmylknolaotan@gmail.com>
2022-08-13 21:52:43 +0100

    Add unfinished calculator

---
+++ 08_calculator/calculator.js
@@ -0,0 +1,37 @@
+const add = function(num1 = 0, num2 = 0) {
+  return num1 + num2;
+};
+
+const subtract = function(num1 = 0, num2 = 0) {
+	return num1 - num2;
+};
+
+const sum = function(arr) {
+  for (item of arr) {
+    return result += item
+  }
+};
+
+const multiply = function(num1 = 0, num2 = 0) {
+
+};
+
+const power = function(num1 = 0, num2 = 0) {
+	
+};
+
+const factorial = function(num) {
+  const result = 1
+  if (num = 0) return 1;
+  total = result * factorial(num - 1)
+};
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
