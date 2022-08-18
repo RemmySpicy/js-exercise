480dce391be6a738bf555b3cfeb5a2f5702c2850
O.Remilekun <remmylknolaotan@gmail.com>
2022-08-18 12:16:11 +0100

    Add completed getTheTitles challenge directory

---
+++ 11_getTheTitles/getTheTitles.js
@@ -0,0 +1,10 @@
+const getTheTitles = function(obj) {
+    let arr = [];
+    for (let i = 0; i < obj.length; i++) {
+        arr.push(obj[i]['title']);
+    }
+    return arr;
+};
+
+// Do not edit below this line
+module.exports = getTheTitles;
