a15e8e876108977eb19a8c209ae65917d870d07b
O.Remilekun <remmylknolaotan@gmail.com>
2022-08-21 16:12:16 +0100

    Add comments in function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,11 @@
 const palindromes = function (str) {
+    // regex pattern that matches all non alphabet and underscore
     const regEx = /[\W_]/gi;
 
+    // convert string to lower case, remove non alphabeths
     const newStr = str.toLowerCase().replace(regEx, "");
 
-    const reversed = newStr.split('').reverse().join('')
+    const reversed = newStr.split('').reverse().join('');
     return newStr == reversed ? true: false;
 };
 
